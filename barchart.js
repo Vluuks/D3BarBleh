7f4583de9c6878b1f7a7de51e1e58e5601823340
Renske Talsma <vluuks@gmail.com>
2018-02-21 19:43:06 +0100

    jeuj

--- barchart.js
+++ barchart.js
@@ -4,7 +4,8 @@ window.onload = getData();
 var globalData;
 var otherData;
 var currentYear;
-var counter;
+var counter = 0;
+var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
 
 function getData() {
 
@@ -37,54 +38,39 @@ function getData() {
             })
             .entries(data);
 
-
-        console.log(JSON.stringify(test));
-        //console.log(test[2011][1].average);
-
-        // test avg of 2011 january
-        test.forEach(function(d){
-
-            // if it's january
-            if(d.key == "1")
-                console.log(d.values);
-        })
-
-    
         globalData = test;
         currentYear = 2011;
-        makeBarchart(2013);
+        makeBarchart(2011);
         setKeyThings();
     });    
 
 }
 
 function setKeyThings() {
-    
-
-    // WHY DOES THIS NOT WORK FFS
     d3.select("body")  
     .on("keydown", function() {
-        console.log("testtt");
-        console.log(d3.event.keyCode);
-        switchYear();
-    })
-}
-
-function switchYear() {
-    
-    counter++;
-    currentYear += (counter % 5 ); // todo met modulos enzo
-    // check input
-    // remake bar shit
-    makeBarchart(currentYear);
+        console.log(d3.event.key);
+        if(d3.event.key == 'ArrowLeft') {
+            counter--;
+            counter < 0 ? counter = 4 : counter = counter; 
+            currentYear = 2011 + (counter % 5 );
+            makeBarchart(currentYear); 
+        }
+        else if(d3.event.key == 'ArrowRight') {
+            counter++;
+            currentYear = 2011 + (counter % 5 ); 
+            makeBarchart(currentYear);
+        }
+    });
 }
 
 function makeBarchart(currentYear) {
 
-     // TODO DELETE SVG IF EXISTS
-
     data = globalData;
     
+    d3.select("#title")
+        .text("Average temperature per month in " + currentYear)
+
     // Set the dimensions of the canvas.
     var margin = {
             top: 20,
@@ -104,22 +90,11 @@ function makeBarchart(currentYear) {
 		
     var y = d3.scale.linear()
 		.range([height, 0])
-        .domain([0, d3.max(otherData, function(d) { // TODO
-            
-            // DETERMINE WHAT THE MAX IS FOR THAT MONTH
-            // THIS IS REALLY CLUNKY
+        .domain([0, d3.max(otherData, function(d) { 
             if(d.year == currentYear) {
                 return d.temperature; 
             }
         })]);
-        console.log(d3.max(otherData, function(d) { // TODO
-            
-            // DETERMINE WHAT THE MAX IS FOR THAT MONTH
-            // THIS IS REALLY CLUNKY
-            if(d.year == currentYear) {
-                return d.temperature; 
-            }
-        }));
 
     // Define the axes.
     var xAxis = d3.svg.axis()
@@ -129,9 +104,14 @@ function makeBarchart(currentYear) {
     var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left")
-        .ticks(20);
+        .ticks(10);
 
+    // Delete the svg if it exists.
+    if(d3.select("#barchartsvg") != undefined ) {
+        d3.select("#barchartsvg").remove();
+    }
 
+    // Add the svg.
     var svg = d3.select("#barchart").append("svg")
         .attr("id", "barchartsvg")
         .attr("width", width + margin.left + margin.right)
@@ -143,14 +123,15 @@ function makeBarchart(currentYear) {
     // Add the Y axis.
     svg.append("g")
         .attr("class", "y axis")
+        .style("stroke-width", "1px")
         .call(yAxis)
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", -43)
+        .attr("y", -50)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
         .attr("fill", "#666666")
-        .text("TESTERONIS");
+        .text("degrees celcius x 10");
 
     // Add X axis, done after bar chart so text is over it instead of under it.
     svg.append("g")
@@ -158,22 +139,69 @@ function makeBarchart(currentYear) {
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
         .selectAll("text")
-        .style("text-anchor", "start")
-        .attr("dx", "1em")
+        .text(function(d, i) {
+            return months[i];
+        })
+        .style("text-anchor", "end")
+        .attr("fill", "#666666")
+        .attr("dx", "-1em")
         .attr("dy", "-.55em")
         .attr("transform", "rotate(-90)")
 
     // Add bar chart.
-    svg.selectAll("bar")
+
+    var barElements = svg.selectAll("bar")
         .data(data)
         .enter()
+        .append("g")
+    
+    barElements
+        .append("text")
+        .text(function(d, i) {
+            // Grab array of temperatures of this month for each year
+            var temperoni;
+            var temp = d.values;
 
+            // Iterate over the years, and get the correct one
+            temp.forEach(function(i){
+
+                if(i.key == currentYear) {
+                    temperoni = y(i.values);
+                }
+            })
+
+            var label = temperoni.toString().substring(0, 6);
+
+            return label;
+        })
+        .style("text-anchor", "middle")
+        .attr("x", function(d) {
+            return 25 + x(+d.key);
+        })
+        .attr("y", function(d) {
+
+            // Grab array of temperatures of this month for each year
+            var temperoni;
+            var temp = d.values;
+
+            // Iterate over the years, and get the correct one
+            temp.forEach(function(i){
+
+                if(i.key == currentYear) {
+                    temperoni = y(i.values);
+                }
+            })
+
+            return temperoni - 5;
+        })
+    
+    barElements
         .append("rect")
         .attr("class", "bar")
         .attr("x", function(d) {
-            console.log("month: " + +d.key);
             return x(+d.key);
         })
+        .attr("fill", "#D3D3D3")
         .attr("width", x.rangeBand())
         .attr("y", function(d) {
 
@@ -193,7 +221,6 @@ function makeBarchart(currentYear) {
         })
         .attr("height", function(d, i) {
 
-            console.log("HEIGHT IS CALLED FOR THE " + i + "TH TIME");
             var temperoni;
 
             // Grab array of temperatures of this month for each year
@@ -203,8 +230,6 @@ function makeBarchart(currentYear) {
             temp.forEach(function(i) {
 
                 if(i.key == currentYear) {
-                    console.log("YEAR MATCH: " + currentYear + " | " + i.key + " AVG: " + i.values);
-                    console.log("final return " +  (height - y(i.values)));
                     temperoni = height - y(i.values);
                 }
             })
